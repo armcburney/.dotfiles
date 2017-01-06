a2f6ca71f5a241c035aa4774117e8ede1d535af8
Andrew R. McBurney <andrewrobertmcburney@gmail.com>
2017-01-06 11:38:20 -0500

    Updated dotfiles.

--- .emacs.d/elpa/sbt-mode-20161202.227/sbt-mode-pkg.el
+++
@@ -1,7 +0,0 @@
-(define-package "sbt-mode" "20161202.227" "Interactive support for sbt projects"
-  '((emacs "24.4"))
-  :url "https://github.com/ensime/emacs-sbt-mode" :keywords
-  '("languages"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
--- .emacs.d/elpa/scala-mode-20161122.2325/scala-mode-pkg.el
+++
@@ -1,5 +0,0 @@
-(define-package "scala-mode" "20161122.2325" "Major mode for editing Scala" 'nil :url "https://github.com/ensime/emacs-scala-mode" :keywords
-  '("languages"))
-;; Local Variables:
-;; no-byte-compile: t
-;; End:
