3fa29fbe55bf52cd5e3e5c077b54452c91c42478
Andrew McBurney <andrewrobertmcburney@gmail.com>
2017-08-24 11:01:18 -0700

    Updated install script.

--- bin/includes/install.rb
+++ bin/includes/install.rb
@@ -26,6 +26,7 @@ module SetUp
 
       # Install zsh
       `sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"`
+      `git clone https://github.com/bhilburn/powerlevel9k.git ~/.oh-my-zsh/custom/themes/powerlevel9k`
 
       # Install other packages
       `sudo easy_install pip`
