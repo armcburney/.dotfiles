c207399523074448a26eacdc23543273b0f9c221
AndrewMcBurney <andrew.mcburney@datadoghq.com>
2018-04-26 18:56:59 -0400

    Minor updates to install scripts.

--- bin/installers/child/pip.rb
+++ bin/installers/child/pip.rb
@@ -4,5 +4,9 @@ require_relative "../package_installer"
 
 module SetUp
   class Pip < PackageInstaller
+    def pre_install!
+      system("pyenv virtualenv #{config[:version]} my-#{config[:version]}")
+      system("pyenv activate my-#{config[:version]}")
+    end
   end
 end
