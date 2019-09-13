43bf308d5685f6feb963f4bc6f92e89528cdeaa4
Andrew McBurney <andrew@mcburney.email>
2019-09-12 20:52:01 -0700

    Created `models/datadog` for scripts that interact with the Datadog API.

---
+++ bin/models/datadog/downtime.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require_relative "../base"
+
+module Model
+  # A representation of a Datadog Downtime in Ruby, for my own scripting purposes.
+  class Downtime < Model::Base
+    # @return [Array[String]]
+    attr_reader :scope
+
+    # @return [DateTime]
+    attr_reader :start_ts
+
+    # @return [DateTime]
+    attr_reader :end_ts
+
+    # @return [Hash]
+    attr_reader :recurrence
+
+    def initialize(
+      scope:,
+      start_ts:,
+      end_ts:,
+      recurrence: nil,
+    )
+      @scope        = scope
+      @start_ts     = start_ts
+      @end_ts       = end_ts
+      @recurrence   = recurrence || default_recurrence
+    end
+
+    private
+
+    def default_recurrence
+      end_reccurrence_ts = start_ts + (4 * 7 * 24 * 60 * 60)
+
+      {
+        "type" => "weeks",
+        "period" => 1,
+        "week_days" => ["Mon", "Tue", "Wed", "Thu", "Fri"],
+        "until_date" => end_reccurrence_ts
+      }
+    end
+  end
+end
---
+++ bin/models/datadog/monitor.rb
@@ -0,0 +1,45 @@
+# frozen_string_literal: true
+
+require_relative "../base"
+
+module Model
+  # A representation of a Datadog Monitor in Ruby, for my own scripting purposes.
+  class Monitor < Model::Base
+    # The type of the monitor, i.e., "metric alert"
+    #
+    # @return [String]
+    attr_reader :type
+
+    # @return [String]
+    attr_reader :name
+
+    # @return [String]
+    attr_reader :message
+
+    # @return [Array[String]]
+    attr_reader :tags
+
+    # The query string to monitor based on, i.e.,
+    #   "avg(last_5m):sum:system.net.bytes_rcvd{host:host0} > 10"
+    #
+    # @return [String]
+    attr_reader :query
+
+    def initialize(
+      type:,
+      name:,
+      message:,
+      tags:,
+      query:,
+    )
+      @type    = type
+      @name    = name
+      @message = message
+
+      # Use a custom tag to ensure it's easy to batch delete the monitors you
+      # created specifically with the API.
+      @tags    = tags + ["github:armcburney"]
+      @query   = query
+    end
+  end
+end
